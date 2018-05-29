efdf729e51d213ce826c4424c2d6c1e1e98bf8fc
abhijitkalta <abhijitkalta@gmail.com>
2018-05-29 12:49:16 +0000

    new changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-   * This is a class
+   * @param  is a class
+   * @return string
    * */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
